1973cbfebf96fbddd76e30b0df8dbecf9e9b410a
Saphyr55 <saphyr0550@gmail.com>
2023-11-24 16:12:10 +0100

    Add unique constraint.

--- src/Entity/Read.php
+++ src/Entity/Read.php
@@ -6,22 +6,31 @@ use App\Repository\ReadRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ReadRepository::class)]
+#[ORM\UniqueConstraint(
+    name: 'reading_unique',
+    columns: ['user_id', 'book_id']
+)]
 class Read
 {
 
     #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
     #[ORM\ManyToOne(targetEntity: User::class)]
     private ?User $user = null;
 
-    #[ORM\Id]
     #[ORM\ManyToOne(targetEntity: Book::class)]
     private ?Book $book = null;
 
     #[ORM\ManyToOne(targetEntity: ReadingStatus::class)]
     private ?ReadingStatus $status = null;
 
-    #[ORM\Column]
-    private ?int $score = null;
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
     public function getUser(): ?User
     {
@@ -38,19 +47,11 @@ class Read
         return $this->status;
     }
 
-    public function getScore(): ?int
-    {
-        return $this->score;
-    }
-
-    public function setScore(?int $score): void
-    {
-        $this->score = $score;
-    }
-
     public function setStatus(?ReadingStatus $status): void
     {
         $this->status = $status;
     }
+
+
 
 }
