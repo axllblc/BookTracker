8ea07b301513c6122f595dd7298b0b7677e40a8f
Axel Leblanc <axel.leblanc@etu.univ-tours.fr>
2023-12-02 00:14:30 +0100

    Role Constants

---
+++ src/Security/RoleConstants.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Security;
+
+class RoleConstants
+{
+    public const ROLE_ADMIN = 'ROLE_ADMIN';
+    public const ROLE_CONTRIBUTOR = 'ROLE_CONTRIBUTOR';
+    public const ROLE_MODERATOR = 'ROLE_MODERATOR';
+}
