b5bfc30306a1c78c468494e2633fee22e4c1e968
tsam42 <theophane.sam [at] proton [dot] me>
2023-12-01 12:00:47 +0100

    Attempt to style user profile page + correct the routes

--- src/Controller/UserProfileController.php
+++ src/Controller/UserProfileController.php
@@ -2,12 +2,8 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\UserType;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,26 +18,8 @@ class UserProfileController extends AbstractController
             dd('TODO: 404');
         }
 
-        return $this->render('user_profile/show.html.twig', [
+        return $this->render('user_profile/index.html.twig', [
             'user' => $user,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_user_profile_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_user_profile_show', ['id' => $user->getId()], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('user_profile/edit.html.twig', [
-            'user' => $user,
-            'form' => $form,
         ]);
     }
 }
