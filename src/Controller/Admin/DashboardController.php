a9d8aa54da12f22124226c2b26d177c6c3ccf6e0
Axel Leblanc <axel.leblanc@etu.univ-tours.fr>
2023-12-03 01:30:11 +0100

    Menu entries are shown / hidden depending on the role of the user

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,6 +8,7 @@ use App\Entity\BookGenre;
 use App\Entity\Emotion;
 use App\Entity\Review;
 use App\Entity\User;
+use App\Security\RoleConstants;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -16,8 +17,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
-
-
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
@@ -32,21 +31,33 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        return [
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
 
-            MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+        // Users section
+        // Allowed users: ROLE_ADMIN, ROLE_MODERATOR
+        if ($this->isGranted(RoleConstants::ROLE_MODERATOR)) {
+            yield MenuItem::section('Users');
+            yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class);
+        }
 
-            // Section users
-            MenuItem::section('Users'),
-            MenuItem::linkToCrud('Users', 'fa fa-user', User::class),
-            MenuItem::linkToCrud('Emotions', 'fa fa-face-smile', Emotion::class),
+        // Books section
+        // Allowed users: ROLE_ADMIN, ROLE_CONTRIBUTOR
+        if ($this->isGranted(RoleConstants::ROLE_CONTRIBUTOR)) {
+            yield MenuItem::section('Books');
+            yield MenuItem::linkToCrud('Books', 'fas fa-book', Book::class);
+            yield MenuItem::linkToCrud('Authors', 'fa fa-user', Author::class);
+            yield MenuItem::linkToCrud('Genres','fa fa-pen', BookGenre::class);
+        }
 
-            // Section books
-            MenuItem::section('Books'),
-            MenuItem::linkToCrud('Books', 'fas fa-book', Book::class),
-            MenuItem::linkToCrud('Authors', 'fa fa-user', Author::class),
-            MenuItem::linkToCrud('Reviews', 'fas fa-star-half-stroke', Review::class),
-            MenuItem::linkToCrud('Genres','fa fa-pen', BookGenre::class)
-        ];
+        // Reviews section
+        // Allowed users: ROLE_ADMIN, ROLE_MODERATOR
+        if ($this->isGranted(RoleConstants::ROLE_MODERATOR)) {
+            yield MenuItem::section('Reviews');
+            yield MenuItem::linkToCrud('Reviews', 'fas fa-star-half-stroke', Review::class);
+        }
+        // Allowed users: ROLE_ADMIN only
+        if ($this->isGranted(RoleConstants::ROLE_ADMIN)) {
+            yield MenuItem::linkToCrud('Emotions', 'fa fa-face-smile', Emotion::class);
+        }
     }
 }
