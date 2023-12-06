081ae3623ad8bfc0e12afb1caa70eee601564d26
tsam42 <theophane dot sam at proton dot me>
2023-12-06 21:25:51 +0100

    Handle email change

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,29 +4,45 @@ namespace App\Controller;
 
 use App\Form\AccountEmailType;
 use App\Form\AccountPasswordType;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/account')]
 class AccountController extends AbstractController
 {
     #[Route('/', name: 'app_account', methods: ['GET', 'POST'])]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher): Response
     {
-        $user = $this->getUser();
-
-        $emailForm = $this->createForm(AccountEmailType::class, $user);
-        $emailForm->handleRequest($request);
-        if ($emailForm->isSubmitted() && $emailForm->isValid()) {
-            $entityManager->flush();
-            return $this->redirectToRoute('app_account', [], Response::HTTP_SEE_OTHER);
+        $user = $userRepository->findOneBy(['email' => $this->getUser()->getUserIdentifier()]);
+        if ($user === null) {
+            dd('Unreachable: user should be fully authentificated');
         }
 
+        $errorMsg = '';
+        $emailForm = $this->createForm(AccountEmailType::class, $user);
+        $emailForm->handleRequest($request);
         $passwordForm = $this->createForm(AccountPasswordType::class, $user);
         $passwordForm->handleRequest($request);
+
+        if ($emailForm->isSubmitted() && $emailForm->isValid()) {
+            $email = $emailForm->get('email')->getData();
+            $email2 = $emailForm->get('email2')->getData();
+
+            if (strcmp($email, $email2) == 0) {
+                if (strcmp($email, $user->getEmail()) != 0) {
+                    $entityManager->flush();
+                    return $this->redirectToRoute('app_account', [], Response::HTTP_SEE_OTHER);
+                }
+            } else {
+                $errorMsg = 'The email addresses do not match.';
+            }
+        }
+
         if ($passwordForm->isSubmitted() && $passwordForm->isValid()) {
             $entityManager->flush();
             return $this->redirectToRoute('app_account', [], Response::HTTP_SEE_OTHER);
@@ -36,6 +52,7 @@ class AccountController extends AbstractController
             'user' => $user,
             'emailForm' => $emailForm,
             'passwordForm' => $passwordForm,
+            'errorMsg' => $errorMsg,
         ]);
     }
 }
