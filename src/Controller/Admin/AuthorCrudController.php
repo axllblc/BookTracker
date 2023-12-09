c49a850c890aa849e2e6d0d92eca9f454b4a8fdd
Axel Leblanc <axel.leblanc@etu.univ-tours.fr>
2023-12-09 18:37:53 +0100

    EasyAdmin: Details pages (author, book)

--- src/Controller/Admin/AuthorCrudController.php
+++ src/Controller/Admin/AuthorCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Author;
 use App\Security\RoleConstants;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
@@ -20,6 +23,12 @@ class AuthorCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return Author::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL);
     }
 
 
