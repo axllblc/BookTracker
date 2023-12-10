f21d12710c554b373deaf44ba52c3a4b4621f6ac
tsam42 <theophane dot sam at proton dot me>
2023-12-10 19:32:32 +0100

    Style homepage

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -6,15 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-// TODO customize this controller!
-
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
     public function index(): Response
     {
         return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
+
         ]);
     }
 }
