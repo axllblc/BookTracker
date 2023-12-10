2b260d5827599feacccdfa4ad309e8c9c37f22d7
tsam42 <theophane dot sam at proton dot me>
2023-12-10 21:08:39 +0100

    Populate the book discovery cards

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    public function index(BookRepository $bookRepository): Response
     {
-        return $this->render('homepage/index.html.twig', [
+        $books = $bookRepository->findAll();
+        $randomKeys = array_rand($books, 3);
+        if ($randomKeys === null) {
+            // #style: It's tempting to make it fail hard if we don't
+            //         have enough books registered in the database.
+            $books = null;
+        } else {
+            $books = array_filter($books,
+                                  fn(int $bookId) => in_array($bookId, $randomKeys),
+                                  ARRAY_FILTER_USE_KEY);
+            shuffle($books);
+        }
 
+        return $this->render('homepage/index.html.twig', [
+            'books' => $books,
         ]);
     }
 }
