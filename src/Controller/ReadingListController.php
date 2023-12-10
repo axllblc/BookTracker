48f1dedf8ae113715278784d22a4200bddb3da9c
tsam42 <theophane dot sam at proton dot me>
2023-12-11 00:41:22 +0100

    Populate the reading list

--- src/Controller/ReadingListController.php
+++ src/Controller/ReadingListController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
 use App\Repository\BookRepository;
+use App\Repository\ReviewRepository;
 use App\Repository\UserRepository;
+use App\Service\ReadService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +15,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class ReadingListController extends AbstractController
 {
     #[Route('/{username}', name: 'app_reading_list')]
-    public function index(string $username, UserRepository $userRepository, BookRepository $bookRepository): Response
+    public function index(string $username,
+                          UserRepository $userRepository,
+                          BookRepository $bookRepository,
+                          ReviewRepository $reviewRepository,
+                          ReadService $readService,): Response
     {
         $user = $userRepository->findOneBy(['username' => $username]);
         if ($user === null) {
@@ -20,21 +27,15 @@ class ReadingListController extends AbstractController
         }
 
         $books = $bookRepository->findAll();
-        $randomKeys = array_rand($books, 3);
-        if ($randomKeys === null) {
-            // #style: It's tempting to make it fail hard if we don't
-            //         have enough books registered in the database.
-            $books = null;
-        } else {
-            $books = array_filter($books,
-                                  fn(int $bookId) => in_array($bookId, $randomKeys),
-                                  ARRAY_FILTER_USE_KEY);
-            shuffle($books);
-        }
+        $books = array_filter($books, fn(Book $book) => $readService->getReadFromBookAndUser($book, $user) !== null);
+        $booksWithReview = array_map(
+            fn(Book $book) => [$book, $reviewRepository->findReviewByBookAndUser($book, $user)],
+            $books,
+        );
 
         return $this->render('reading_list/index.html.twig', [
             'user' => $user,
-            'books' => $books,
+            'booksWithReview' => $booksWithReview,
         ]);
     }
 }
