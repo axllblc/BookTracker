62bb307d44a1cb11e1117f28352dd04fd0ff91e7
Axel Leblanc <axel.leblanc@etu.univ-tours.fr>
2023-12-11 16:50:38 +0100

    Fixed issue with Bootstrap JS

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,7 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 import './bootstrap';
+import 'bootstrap';
 
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
